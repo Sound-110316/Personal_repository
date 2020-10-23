ca6112c090b9ce31896146615747d1882954525b
DESKTOP-N4TJ7SA\yamah <j292155c@myjuen.jp>
2020-10-23 17:13:06 +0900

    excelファイルのデータをfitsファイルにします。

--- X2F.py
+++ X2F.py
@@ -3,14 +3,14 @@ import os
 import pandas as pd
 import numpy as np
 
-template = input("テンプレート名 : ")
+template = input("テンプレート名(xxx.fits) : ")
 hdulist = fits.open(template)
 hdu = hdulist[0]
 data = hdu.data
 headerT = hdu.header
 
 #input file name
-input_file_name = input("ファイル名 : ") # 'planck_Temp.xlsx'
+input_file_name = input("ファイル名(YYY.xlsx) : ") # 'planck_Temp.xlsx'
 sheetNo = input("シートの番号(ex. 0) : ")
 if sheetNo == "":
     sheetNo = 0
@@ -23,14 +23,14 @@ input_book = pd.ExcelFile(input_file_name)
 #sheet_namesメソッドでExcelブック内の各シートの名前をリストで取得できる
 input_sheet_name = input_book.sheet_names
 
-#DataFrameとして一つ目のsheetを読込
-input_sheet_df = input_book.parse(input_sheet_name[sheetNo]).iloc[::-1]
+#DataFrameとして"sheetNo"番目のsheetを読込
+input_sheet_df = input_book.parse(input_sheet_name[sheetNo],header=None).iloc[::-1]
 # 0をNaNで置換
 input_sheet_df = input_sheet_df.replace(0,np.nan)
-
-
+# 温度平均を出力
+print("mean : ",round(np.nanmean(input_sheet_df.values.tolist()),2))
+# 書き出しファイルの名前を決める
 outfile = os.path.splitext(input_file_name)[sheetNo]
-
 
 hdu = fits.PrimaryHDU(data=input_sheet_df)
 
@@ -39,8 +39,8 @@ hdu.header['NAXIS1'] = len(input_sheet_df.columns)
 hdu.header['NAXIS2'] = len(input_sheet_df)
 hdu.header["CDELT1"] = headerT["CDELT1"]*(headerT["NAXIS1"]/hdu.header['NAXIS1'])
 hdu.header["CDELT2"] = headerT["CDELT2"]*(headerT["NAXIS2"]/hdu.header['NAXIS2'])
-hdu.header["CRPIX1"] = (headerT["CRPIX1"]-1)*(hdu.header['NAXIS1']/headerT["NAXIS1"])+1
-hdu.header["CRPIX2"] = (headerT["CRPIX2"]-1)*(hdu.header['NAXIS2']/headerT["NAXIS2"])+1
+hdu.header["CRPIX1"] = (headerT["CRPIX1"]-0.5)*(hdu.header['NAXIS1']/headerT["NAXIS1"])+0.5
+hdu.header["CRPIX2"] = (headerT["CRPIX2"]-0.5)*(hdu.header['NAXIS2']/headerT["NAXIS2"])+0.5
 hdu.header["BLANK"]  = 0.0
 
 # 元のfitsにあったヘッダーを入れる
